3a92ab7e3e90bc7b6e60194203ff35e87f837240
damianstaro <damian@Damians-MacBook-Pro.local>
2017-04-28 15:41:50 -0400

    combined commit
    reorder stuff
    
    add some stuff
    
    f7

--- test.py
+++ test.py
@@ -2,20 +2,28 @@ def f1():
     print('f1')
 
 
+def f2():
+    print('f2')
+
+
 def f3():
     print('f3')
+
+
+def f4():
+    print('f2')
 
 
 def f5():
     print('f5')
 
 
-def f2():
-    print('f2')
+def f6():
+    print('f6')
 
 
-def f4():
-    print('f2')
+def f7():
+    print('f7')
 
 
 def main():
